3b0f0a937cc3aa295c358f22d26993bedc2a5208
Nuno Domingues <ndomingues10@gmail.com>
2024-03-25 22:32:07 +0000

    chore: update serviceAPI

--- src/pt/nomorealex/medmanagement/model/ServiceAPI.java
+++ src/pt/nomorealex/medmanagement/model/ServiceAPI.java
@@ -3,7 +3,7 @@ package pt.nomorealex.medmanagement.model;
 import pt.nomorealex.medmanagement.model.fsm.StatesAPI;
 
 public class ServiceAPI {
-    public ServiceAPI() {
+public ServiceAPI() {
     }
     public String get(){
         return "entry";
